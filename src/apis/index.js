0eb50540bd60980250a08291b331128665bd486f
Anirban Dey <116617392+anirbandey1@users.noreply.github.com>
2023-07-29 11:11:52 +0530

    Add all Invidious Public Instances
    Add all invidious public instances manually. Plan to use api.invidious.io to update instances automatically

--- src/apis/index.js
+++ src/apis/index.js
@@ -5,8 +5,8 @@ const getInvidiousInstances = ()=> {
 
 }
 
-const invidious_instances = ["invidious.snopyta.org","yt.floss.media","yewtu.be",]
-let invidious_instance = invidious_instances[1]
+const invidious_instances = ["invidious.snopyta.org","yewtu.be","vid.puffyan.us","yt.artemislena.eu","invidious.projectsegfau.lt","y.com.sb","invidious.tiekoetter.com","invidious.slipfox.xyz","invidious.privacydev.net","vid.priv.au","iv.melmac.space","iv.ggtyler.dev","invidious.lunar.icu","invidious.0011.lt","inv.zzls.xyz","yt.floss.media","inv.tux.pizza","not-ytp.blocus.ch","invidious.protokolla.fi","onion.tube","inv.in.projectsegfau.lt","inv.citw.lgbt","inv.makerlab.tech","yt.oelrichsgarcia.de"]
+let invidious_instance = invidious_instances[2]
 
 const invidious = axios.create({
   baseURL: "https://"+invidious_instance

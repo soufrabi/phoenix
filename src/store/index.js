0e92481fbf93785c80d1d8a5f9b53f6cb400d88e
Anirban Dey <116617392+anirbandey1@users.noreply.github.com>
2023-07-29 11:11:52 +0530

    combineReducer and invidious instance change

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
 
 import authSlice from "./auth-slice.js"
 import cartSlice from "./cart-slice.js";
@@ -10,18 +11,38 @@ import playlistsSlice from "./playlists.js";
 import historySlice from "./history.js"
 import debugSlice from "./debug.js";
 
-const store = configureStore({
-  reducer: {
-    auth: authSlice.reducer,
-    cart: cartSlice.reducer,
-    page: pageSlice.reducer,
-    watchSuggestions: watchSuggestionsSlice.reducer,
-    videoPlayerInfo: videoPlayerInfoSlice.reducer,
-    searchResults: searchResultsSlice.reducer,
-    playlists: playlistsSlice.reducer,
-    history: historySlice.reducer,
-    debug: debugSlice.reducer,
+
+const combinedReducers = combineReducers({
+  auth: authSlice.reducer,
+  cart: cartSlice.reducer,
+  page: pageSlice.reducer,
+  watchSuggestions: watchSuggestionsSlice.reducer,
+  videoPlayerInfo: videoPlayerInfoSlice.reducer,
+  searchResults: searchResultsSlice.reducer,
+  playlists: playlistsSlice.reducer,
+  history: historySlice.reducer,
+  debug: debugSlice.reducer,
+
+})
+
+const rootReducer = (state, action) => {
+  if(action.payload == "RESET"){
+    console.log("RESET action")
   }
+
+  if (action.payload == "PRINT_STATE"){
+    console.log("PRINT_STATE action")
+    console.log(state)
+  }
+
+  return combinedReducers(state,action)
+
+
+}
+
+
+const store = configureStore({
+  reducer: rootReducer
 
 })
 

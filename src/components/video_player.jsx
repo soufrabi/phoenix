adce6f5d6c23ee368b0faa1e8809554fb8415928
Anirban Dey <code@soufrabi.com>
2024-01-11 10:37:04 +0530

    remove mistakenly added plaintext in svg

--- src/components/video_player.jsx
+++ src/components/video_player.jsx
@@ -359,8 +359,7 @@ const VideoPlayer = () => {
                 <svg className="play-icon" viewBox="0 0 24 24">
                   <path fill="currentColor" d="M8,5.14V19.14L19,12.14L8,5.14Z" />
                 </svg>
-                <svg className="pause-icon" viewBox="0 0 24 24">me` prop on a DOM element. If you intentionally want it to appear in the DOM as a custom attribute, spell it as lowercase `currenttime` instead. If you accidentally passed it from a parent component, remove it from the DOM element.
-                  video
+                <svg className="pause-icon" viewBox="0 0 24 24">
                   <path fill="currentColor" d="M14,19H18V5H14M6,19H10V5H6V19Z" />
                 </svg>
               </button>

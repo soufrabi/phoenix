d2f5defc9100f615dff1c88052b182a8188c35c3
Anirban Dey <code@soufrabi.com>
2024-01-11 21:25:42 +0530

    enable vite-plugin-pwa in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,39 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/phoenix",
-  plugins: [react()],
+    base: "/",
+    plugins: [
+        react(),
+        VitePWA({
+            /* enable sw on development */
+            devOptions: {
+                enabled: true
+                /* other options */
+            },
+            registerType: 'autoUpdate',
+            manifest: {
+                icons: [
+                    {
+                        src: 'pwa-192x192.png', // <== don't add slash, for testing
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: '/pwa-512x512.png', // <== don't remove slash, for testing
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'pwa-512x512.png', // <== don't add slash, for testing
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: ['any', 'maskable'], // testing new type declaration
+                    },
+                ],
+            }
+        }),
+    ],
 })

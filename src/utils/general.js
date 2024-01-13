8c21dd425129747edba41526d9b32fc43ed6c2aa
Anirban Dey <code@soufrabi.com>
2024-01-13 21:58:16 +0530

    add random number functions

--- src/utils/general.js
+++ src/utils/general.js
@@ -35,6 +35,34 @@ const liesWithinBoundingRect = (x,y,rect)=>{
     }
 }
 
+/**
+ * Returns a random number between min (inclusive) and max (exclusive)
+ */
+const getRandomArbitrary = (min, max)=>{
+    return Math.random() * (max - min) + min;
+}
 
-export { getDeviceInfo,isTouchEnabled, isOrientationPortrait , liesWithinBoundingRect }
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive).
+ * The value is no lower than min (or the next integer greater than min
+ * if min isn't an integer) and no greater than max (or the next integer
+ * lower than max if max isn't an integer).
+ * Using Math.round() will give you a non-uniform distribution!
+ */
+const getRandomInt = (min, max)=>{
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+
+export {
+    getDeviceInfo,
+    isTouchEnabled,
+    isOrientationPortrait,
+    liesWithinBoundingRect,
+    getRandomArbitrary,
+    getRandomInt
+}
 

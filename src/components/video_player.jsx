f495c59cb885af848fc6b7ca57703585168c265e
Anirban Dey <code@soufrabi.com>
2024-01-13 17:37:19 +0530

    do'nt show minplayer,theatre in touch devices

--- src/components/video_player.jsx
+++ src/components/video_player.jsx
@@ -565,16 +565,19 @@ const VideoPlayer = () => {
               <button onClick={handlePlaybackRateButtonClick} className="speed-btn wide-btn">
                 {playbackRate}x
               </button>
-              <button className="mini-player-btn" onClick={toggleMiniplayerMode}>
-                <svg viewBox="0 0 24 24">
-                  <path fill="currentColor" d="M21 3H3c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H3V5h18v14zm-10-7h9v6h-9z" />
-                </svg>
-              </button>
 
             { !touchEnabled &&
-              <button className="theater-btn" onClick={toggleTheatreMode}>
-                <TheatreIcons tall={true} />
-              </button>
+              <>
+                <button className="mini-player-btn" onClick={toggleMiniplayerMode}>
+                  <svg viewBox="0 0 24 24">
+                    <path fill="currentColor" d="M21 3H3c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H3V5h18v14zm-10-7h9v6h-9z" />
+                  </svg>
+                </button>
+
+                <button className="theater-btn" onClick={toggleTheatreMode}>
+                  <TheatreIcons tall={true} />
+                </button>
+              </>
             }
               <button className="full-screen-btn" onClick={toggleFullScreenMode}>
                 <FullscreenIcons fullscreen={true} />

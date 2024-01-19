033fa6f77e049d45e7014e68f375b64274ea9d5c
Anirban Dey <code@soufrabi.com>
2024-01-19 23:03:59 +0530

    removeEvent listener in useEffect

--- src/components/frontend.jsx
+++ src/components/frontend.jsx
@@ -62,6 +62,11 @@ const FrontEnd = () => {
       window.matchMedia("(orientation: portrait)").addEventListener("change", handleOrientationChange)
       invidious_api.getLatestInstanceList()
 
+      return ()=>{
+        window.matchMedia("(orientation: portrait)").removeEventListener("change", handleOrientationChange)
+
+      }
+
   },[])
 
   return (
